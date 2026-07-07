006b1d65fc499cfb584e31e671f8173f0970550d
Dev-Atta <meatta001@gmail.com>
2026-07-08 02:32:17 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,85 +1,25 @@
-// document.getElementById("count-el").innerText = count;
+let inputel = document.getElementById("todo-input");
+let listel = document.getElementById("todo-list");
 
+function addTodo() {
+  let task = inputel.value;
 
-// THE DOM
-// document object model
-// document.getElementById("count-el").innerText = 5;
+  if (task === "") {
+    return;
+  }
 
+  let li = document.createElement("li");
+  li.innerText = task;
 
+  let delbtn = document.createElement("button");
+  delbtn.innerText = "X";
+  delbtn.className = "del-btn";
+  delbtn.onclick = function () {
+    li.remove();
+  };
 
+  li.appendChild(delbtn);
+  listel.appendChild(li);
 
-// let count = 0;
-// count += 1 ;
-
-//  let firstBatch = 5;
-//  let secondBatch = 7;
-//  let count = firstBatch + secondBatch;
-
-
-// let myAge = 25;
-// let humanDogRatio = 7;
-// let dogAge = myAge * humanDogRatio;
-// console.log(dogAge);
-
-
-// function countdown() {
-//     console.log(5);
-//     console.log(4);
-//     console.log(3);
-//     console.log(2);
-//     console.log(1);   
-// }
-
-
-// function fortytwo() {
-//     console.log(42);
-// }
-// let lap = 0;
-// function LAPCOUNT() {
-//     lap += 1;
-//     document.getElementById("countdown-el").innerText = lap;
-// }
-
-
-
-
-let countel = document.getElementById("count-el");
-let saveel = document.getElementById("save-el");
-
-let count = 0;
-function increment() {
-  count += 1;
-   countel.innerText = count;
+  inputel.value = "";
 }
-
-function save() {
-  let savenum = count;
-  saveel.innerText += " " + savenum + " - ";
-}
-
-
-
-// function user() {
-//     let username = "John";
-//     document.getElementById("user-el").innerText = username;
-//     let msg ="Welcome back, ";
-
-//     let msgtousr = msg + username;
-//     document.getElementById("msg-el").innerText = msgtousr;
-//     console.log(msgtousr);
-  
-// }
-// String always win between text and string
-
-// let welcomeel = document.getElementById("welcome-el");
-
-// let name = "John";
-// let greeting = "Hey yooo wsssup , ";
-
-// welcomeel.innerText = greeting + name;
-
-// welcomeel.innerText += " 👋";
-
-
-
-
